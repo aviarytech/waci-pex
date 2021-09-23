2a21a3cf006ace7142071d6235ef82d44c3eda85
Brian Richter <brianorwhatever@gmail.com>
2021-09-23 13:27:16 -0700

    with ids

--- src/__tests__/__fixtures__/credentials/index.ts
+++ src/__tests__/__fixtures__/credentials/index.ts
@@ -1,3 +1,4 @@
 import vax from "./vax.json";
 import simple from "./simple.json";
-export default { vax, simple };
+import vaxWithIds from "./vax-with-ids.json";
+export default { vax, simple, vaxWithIds };
